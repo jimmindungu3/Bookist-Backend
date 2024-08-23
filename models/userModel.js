37f906bc0d849c6e449c612038707dc7d47feb6d
james <jimmindungu3@gmail.com>
2024-08-23 17:03:37 +0300

    create eventmodel

--- models/userModel.js
+++ models/userModel.js
@@ -1,12 +1,21 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  fullName: {
+  firstName: {
     type: String,
-    minlength: [6, "Full name must be at least 6 characters long."],
-    maxlength: [30, "Full name cannot be more than 30 characters long"],
-    required: [true, "Name is required"],
+    set: (v) => v.charAt(0).toUpperCase() + v.slice(1).toLowerCase(), // Transform Name to Title Case
+    trim: true, // remove leading and trailing white spaces. *** done before length checks are done ***
+    required: [true, "Firstname is required"],
+    minlength: [3, "First name must be at least 3 characters long."],
+    maxlength: [10, "First name cannot be more than 10 characters long"],
+  },
+  lastName: {
+    type: String,
+    set: (v) => v.charAt(0).toUpperCase() + v.slice(1).toLowerCase(), // Transform Name to Title Case
     trim: true,
+    required: [true, "Lastname is required"],
+    minlength: [3, "Last name must be at least 3 characters long."],
+    maxlength: [10, "Last name cannot be more than 10 characters long"],
   },
   email: {
     type: String,
@@ -16,7 +25,7 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     match: [/^\S+@\S+\.\S+$/, "Please use a valid email address."],
   },
-  tel: {
+  telNo: {
     type: String,
     required: [true, "Mobile number is required"],
     validate: {
@@ -26,6 +35,13 @@ const userSchema = new mongoose.Schema({
       message: (props) =>
         `${props.value} is not a valid mobile number. Phone number should be 10 digits long.`,
     },
+  },
+  password: {
+    type: String,
+    minlength: [8, "Password must be at least 8 characters long."],
+    maxlength: [15, "Password cannot be more than 15 characters long"],
+    required: [true, "Name is required"],
+    trim: true,
   },
   // Add other fields as needed
 });
