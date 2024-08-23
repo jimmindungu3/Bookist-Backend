3b0a7901b52e723ec8340ad71c179d40d9dd767a
james <jimmindungu3@gmail.com>
2024-08-23 21:02:05 +0300

    create booking model

---
+++ models/bookingModel.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const bookingSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  eventId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Event",
+    required: true,
+  },
+  bookingDate: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Booking', bookingSchema)
