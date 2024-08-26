c8bd1b27985a1344c17b56ca722bdde214882be6
james <jimmindungu3@gmail.com>
2024-08-26 09:24:48 +0300

    configure port variable

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { connectToDb } = require("./config/db");
+require("dotenv").config();
 const cors = require("cors");
 const userRoutes = require("./routes/userRoutes");
 const eventRoutes = require("./routes/eventRoutes");
 const bookingRouter = require("./routes/bookingRoutes");
 
+const PORT = process.env.PORT || 3000
 const app = express();
 
 // Middleware
@@ -19,7 +21,7 @@ app.use(bookingRouter);
 // Connect to the database and start the server
 connectToDb((err) => {
   if (!err) {
-    app.listen(3000);
+    app.listen(PORT);
   } else {
     console.error("Error connecting to database");
   }
