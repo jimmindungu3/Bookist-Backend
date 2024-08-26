478ac48a15685d5c1c06664042dfbcda00cbcef8
james <jimmindungu3@gmail.com>
2024-08-26 09:10:05 +0300

    install dotenv, configure atlas db

--- config/db.js
+++ config/db.js
@@ -1,16 +1,21 @@
-// db.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const db_username = process.env.DB_USERNAME;
+const db_password = process.env.DB_PASSWORD;
+const URI = `mongodb+srv://${db_username}:${db_password}@bookist.udmm8.mongodb.net/?retryWrites=true&w=majority&appName=Bookist`;
 
 module.exports = {
   connectToDb: (cb) => {
-    mongoose.connect('mongodb://localhost:27017/bookist')
-    .then(() => {
-      console.log('Connected to MongoDB');
-      cb();
-    })
-    .catch((err) => {
-      console.error('Error connecting to MongoDB:', err);
-      cb(err);
-    });
-  }
+    mongoose
+      .connect(URI)
+      .then(() => {
+        console.log("Connected to MongoDB");
+        cb();
+      })
+      .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+        cb(err);
+      });
+  },
 };
